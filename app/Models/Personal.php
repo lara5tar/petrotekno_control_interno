c0b63b02888af41c264c73192ae2ff04a8806d63
Eder J. Bravo <a2133330155@alumnos.uat.edu.mx>
2025-07-17 13:22:02 -0600

    fix(style): Laravel Pint auto-fix - Personal model

--- app/Models/Personal.php
+++ app/Models/Personal.php
@@ -2,15 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Personal extends Model
 {
-    use SoftDeletes, HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'personal';
 
